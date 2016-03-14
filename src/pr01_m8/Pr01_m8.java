89485bf7b4d05e58e55f2ce7948936c18b45e366
alum-01 <alum-01@A11-08>
2016-03-14 17:25:58 +0100

    versio 0.2 del projecte

--- src/pr01_m8/Pr01_m8.java
+++ src/pr01_m8/Pr01_m8.java
@@ -20,7 +20,9 @@ public class Pr01_m8 {
             bw.write(" </body>"); 
             bw.newLine(); 
             bw.write("</html>"); 
-            bw.newLine(); 
+            bw.newLine();
+            bw.write("<h1>DAW2 m08uf4pr2</h1>");
+            bw.newLine();
             bw.close(); 
             } 
         } 
